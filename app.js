0ff1e261ba3fc9024c10ddac704ac576ff3ff7fc
Alfred Madere <alfred@stevemadere.com>
2021-05-05 21:17:21 -0500

    node modules gone, env added

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ var testRouter = require("./routes/test");
 var loggerRouter = require("./routes/logger");
 
 var app = express();
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
